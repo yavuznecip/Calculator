3c3da80868035453732549013d38e2b7324a3274
necip <necip@DESKTOP-957VG9D>
2021-12-27 13:36:07 +0300

    Proje dosyası ekle.

---
+++ HesapMak/HesapMak.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double n1, n2, result;
+	char op;
+	cout << "Enter the first number: \n";
+	cin >> n1;
+	cout << "Enter the operator: \n";
+	cin >> op;
+	cout << "Enter the second number: \n";
+	cin >> n2;
+
+	if (op == '+')
+		cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
+	else if (op == '-')
+		cout << n1 << " - " << n2 << " = " << n1 - n2 << endl;
+	else if (op == '*')
+		cout << n1 << " * " << n2 << " = " << n1 * n2 << endl;
+	else if (op == '/')
+		cout << n1 << " / " << n2 << " = " << n1 / n2 << endl;
+	else
+		cout << "Invalid operator.";
+};
+
